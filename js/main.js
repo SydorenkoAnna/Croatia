a0e3a5453b4bd830b80b0732e640b5aa3ffda0c4
SydorenkoAnna <72403676+SydorenkoAnna@users.noreply.github.com>
2020-10-16 19:10:30 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-$(function(){
+$(document).ready(function() {
+
 	$('.attractions__gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
+				delegate: 'a',
+				type: 'image',
+				tLoading: 'Loading image #%curr%...',
+				mainClass: 'mfp-img-mobile',
+				gallery: {
+					enabled: true,
+					navigateByImgClick: true,
+					preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+				},
+			});
+
+	$('.menu__btn').click(function(event) {
+		$('.menu__btn, .menu__list').toggleClass('active');
+		$('body').toggleClass('lock')
+	});
+
+	$('.menu__list-link').click(function(event) {
+		$('.menu__btn, .menu__list').toggleClass('active');
+		$('body').removeClass('lock')
 	});
 });
